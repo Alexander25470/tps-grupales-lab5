5d89be8196edf7bde08d85bc17a2117613ef12a5
Alexander Popp <alexanderpopp99@gmail.com>
2022-04-15 19:02:58 -0300

    tp 2

--- Tarea2_Grupo4/src/packageTest/MatrizAdyacenciaTest.java
+++ Tarea2_Grupo4/src/packageTest/MatrizAdyacenciaTest.java
@@ -8,7 +8,7 @@ import frgp.utn.edu.ar.main.MatrizAdyacencia;
 
 class MatrizAdyacenciaTest {
 	
-	MatrizAdyacencia _matrizAdyacencia = new MatrizAdyacencia(5);
+	MatrizAdyacencia _matrizAdyacencia = new MatrizAdyacencia(4);
 	
 	@Test
 	void agregarElementoTest() 
@@ -46,22 +46,66 @@ class MatrizAdyacenciaTest {
 	void contarRelacionesTest() 
 	{
 		_matrizAdyacencia.agregarElemento(2, 3);
-		_matrizAdyacencia.agregarElemento(1, 4);
+		_matrizAdyacencia.agregarElemento(1, 3);
 		_matrizAdyacencia.agregarElemento(1, 2);
 		int resultado = _matrizAdyacencia.getCantidadElementos();
 		assertEquals(3, resultado);
 	}
 	
 	@Test
-	void existenTodosLosElementosTest() {}
+	void existenTodosLosElementosTest() {
+		_matrizAdyacencia = new MatrizAdyacencia(2);
+		_matrizAdyacencia.agregarElemento(0, 0);
+		_matrizAdyacencia.agregarElemento(0, 1);
+		_matrizAdyacencia.agregarElemento(1, 1);
+		
+		int originales = 0;
+		int simetricos = 0;
+		//originales
+		originales += _matrizAdyacencia.existeElemento(0, 0)?1:0;
+		originales += _matrizAdyacencia.existeElemento(0, 1)?1:0;
+		originales += _matrizAdyacencia.existeElemento(1, 1)?1:0;
+		
+		//simetricos
+		simetricos += _matrizAdyacencia.existeElemento(0, 0)?1:0;
+		simetricos += _matrizAdyacencia.existeElemento(1, 0)?1:0;
+		simetricos += _matrizAdyacencia.existeElemento(1, 1)?1:0;
+		
+		assertEquals(true, originales==3 && simetricos == 3);
+	}
 	
 	@Test
-	void agregarElementoFilaNegativaTest() {}
+	void agregarElementoFilaNegativaTest() {
+		boolean ocurrioExcepcion = false;
+		try {			
+			_matrizAdyacencia.agregarElemento(-1, 1);
+		} catch (Exception ex){
+			ocurrioExcepcion = true;
+		}
+		assertEquals(true, ocurrioExcepcion);
+	}
 	
 	@Test
-	void agregarElementoColumnaNegativaTest() {}
+	void agregarElementoColumnaNegativaTest() {
+		boolean ocurrioExcepcion = false;
+		try {			
+			_matrizAdyacencia.agregarElemento(1, -1);
+		} catch (Exception ex){
+			ocurrioExcepcion = true;
+		}
+		assertEquals(true, ocurrioExcepcion);
+	}
 	
 	@Test
-	void agregarElementoFueraDeRangoTest() {}
+	void agregarElementoFueraDeRangoTest() {
+		_matrizAdyacencia = new MatrizAdyacencia(2);
+		boolean ocurrioExcepcion = false;
+		try {			
+			_matrizAdyacencia.agregarElemento(3, 3);
+		} catch (Exception ex){
+			ocurrioExcepcion = true;
+		}
+		assertEquals(true, ocurrioExcepcion);
+	}
 	
 }
